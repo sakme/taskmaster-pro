69bdb1cea6d7fa5ecdcc3b1a315540aabd2fb319
sakme <alan@akme.us>
2021-11-23 16:12:05 -0500

    added trash drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,16 +190,16 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    // console.log("activate", this);
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    // console.log("deactivate", this);
   }, 
   over: function(event) {
-    console.log("over", event.target);
+    // console.log("over", event.target);
   },
   out: function(event) {
-    console.log("out", event.target);
+    // console.log("out", event.target);
   },
   update: function(event) {
     // array to store the task data
@@ -233,6 +233,21 @@ $(".card .list-group").sortable({
     saveTasks();
   }
 });
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
 
 // load tasks for the first time
 loadTasks();
